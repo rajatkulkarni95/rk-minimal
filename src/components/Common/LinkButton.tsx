d1cf2f2c6d6206fde3b79bc469ce50b5c80bc919
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-02-22 22:54:23 +0530

    no-referrer

--- src/components/Common/LinkButton.tsx
+++ src/components/Common/LinkButton.tsx
@@ -9,6 +9,7 @@ export const PrimaryLinkButton = ({ href, text, classes }: TProps) => {
     <a
       href={href}
       target="_blank"
+      rel="noreferrer"
       className="py-1 px-3 rounded-md text-xs bg-zinc-300 text-zinc-900 border border-zinc-500 hover:bg-zinc-100"
     >
       {text}
@@ -21,6 +22,7 @@ export const SecondaryLinkButton = ({ href, text, classes }: TProps) => {
     <a
       href={href}
       target="_blank"
+      rel="noreferrer"
       className="py-1 px-3 rounded-md text-xs bg-zinc-800 text-zinc-50 border border-zinc-500 hover:bg-zinc-700"
     >
       {text}
