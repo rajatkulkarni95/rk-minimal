ec92f94cec1464a3c91aa5ae48488795cdc44b96
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-02-26 15:42:33 +0530

    Fix footer

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -2,18 +2,30 @@ import { links } from "../../data/links";
 
 const Footer = () => {
   return (
-    <footer className="h-12 bg-zinc-800 flex items-center py-2 px-6 sticky justify-between bottom-0 opacity-0 animate-fadeIn [animation-delay:2000ms]">
-      {links.map((link) => (
+    <footer className="h-12 bg-zinc-900 shadow-md flex items-center py-2 px-6 sticky justify-between bottom-0 opacity-0 animate-fadeIn mt-auto [animation-delay:2000ms]">
+      <aside>
         <a
-          href={link.url}
-          key={link.id}
+          href={links[0].url}
           target="_blank"
           rel="noreferrer"
-          className="text-xs text-zinc-200 border-dashed border-b border-zinc-500"
+          className="text-xs text-zinc-400 hover:text-zinc-50 md:ml-6"
         >
-          {link.name}
-        </a>
-      ))}
+          {links[0].name}
+        </a>{" "}
+      </aside>
+      <div>
+        {links.slice(1).map((link) => (
+          <a
+            href={link.url}
+            key={link.id}
+            target="_blank"
+            rel="noreferrer"
+            className="text-xs text-zinc-400 mr-6 md:mr-8 hover:text-zinc-50"
+          >
+            {link.name}
+          </a>
+        ))}
+      </div>
     </footer>
   );
 };
