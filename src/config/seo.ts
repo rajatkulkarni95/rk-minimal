582eae62b555d8369483e21766b0e64ac1b408b7
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-02-26 22:41:52 +0530

    Seo (#1)
    * Favicons and more
    
    * Add next-seo
    
    * Default SEO

---
+++ src/config/seo.ts
@@ -0,0 +1,49 @@
+export const baseUrl =
+  process.env.NODE_ENV === "production" ? "https://rk-minimal.vercel.app" : "";
+export const baseEmail = "rajatkulkarni95@gmail.com";
+
+export const defaultSEO = {
+  title: "Rajat Kulkarni",
+  description: "Frontend focused Software Engineer. Okayish at things",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: baseUrl,
+    site_name: "Rajat Kulkarni",
+    images: [
+      {
+        url: `${baseUrl}/static/og/default.png`,
+        alt: "Rajat Kulkarni",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@JokingTwitter",
+    site: "@JokingTwitter",
+    cardType: "summary_large_image",
+  },
+};
+
+// interface SEOProps {
+//   title?: string;
+//   description?: string;
+//   image?: string;
+//   url?: string;
+// }
+
+// export function extendSEO(options: SEOProps) {
+//   const images = options.image
+//     ? [{ url: `${baseUrl}/static/${options.image}` }]
+//     : defaultSEO.openGraph.images;
+
+//   return {
+//     ...defaultSEO,
+//     ...options,
+//     url: `${baseUrl}/${options.url}`,
+//     openGraph: {
+//       ...defaultSEO.openGraph,
+//       images,
+//       url: `${baseUrl}/${options.url}`,
+//     },
+//   };
+// }
