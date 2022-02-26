1e77658af92bae6ccb43ca26b85249a3af52b2a7
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-02-26 15:47:48 +0530

    Increase animation time on mobile for footer, space decisions

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -2,13 +2,13 @@ import { links } from "../../data/links";
 
 const Footer = () => {
   return (
-    <footer className="h-12 bg-zinc-900 shadow-md flex items-center py-2 px-6 sticky justify-between bottom-0 opacity-0 animate-fadeIn mt-auto [animation-delay:2000ms]">
+    <footer className="h-12 bg-zinc-900 shadow-md flex items-center py-2 px-6 sticky justify-center md:justify-between bottom-0 opacity-0 animate-fadeIn mt-auto [animation-delay:2500ms] md:[animation-delay:2000ms]">
       <aside>
         <a
           href={links[0].url}
           target="_blank"
           rel="noreferrer"
-          className="text-xs text-zinc-400 hover:text-zinc-50 md:ml-6"
+          className="text-xs text-zinc-400 hover:text-zinc-50 mr-6 md:ml-6"
         >
           {links[0].name}
         </a>{" "}
