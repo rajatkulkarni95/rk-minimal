0df547e412f620128bd392c6701ca6ef98c11a71
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-02-26 20:13:02 +0530

    Rudimentary solution of header circle animation

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,7 +1,35 @@
 const Header = () => (
-  <header className="h-16 w-full flex py-2 px-4">
-    {/* <span className="w-12 h-12 rounded-full bg-slate-200" /> */}
+  <header className="h-20 w-full flex py-2 px-4 ">
+    <div className="h-20 w-20">
+      <Circle depth={4} delay={0} style={{ bottom: 0 }} />
+      <Circle depth={3} delay={1} style={{ bottom: "56px" }} />
+      <Circle depth={2} delay={2} style={{ bottom: "96px" }} />
+      <Circle depth={1} delay={3} style={{ bottom: "120px" }} />
+      <Circle depth={0.5} delay={4} style={{ bottom: "132px" }} />
+    </div>
   </header>
+);
+
+type TCircleProps = {
+  depth: number;
+  delay: number;
+  style: object;
+};
+
+const Circle = ({ depth, delay, style }: TCircleProps) => (
+  <div
+    className="circle"
+    style={{
+      width: `${depth * 16}px`,
+      height: `${depth * 16}px`,
+      animationName: "revealCircle",
+      animationDelay: `${delay * 1000}ms`,
+      margin: "0 auto",
+      ...style,
+    }}
+  >
+    <div className="cover" />
+  </div>
 );
 
 export default Header;
