7e6ac4345493f02178755325e7650026f92e0f1f
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-02-26 22:41:25 +0530

    Default SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from "next";
+import { DefaultSeo } from "next-seo";
 import MainContainer from "../components/MainContainer";
+import { defaultSEO } from "../config/seo";
 
 const Home: NextPage = () => {
-  return <MainContainer />;
+  return (
+    <>
+      <DefaultSeo {...defaultSEO} />
+      <MainContainer />
+    </>
+  );
 };
 
 export default Home;
