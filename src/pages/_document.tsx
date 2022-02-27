e7d2d0b78d2f5c3dea89eb3d364174196c775931
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-02-27 15:28:11 +0530

    Fix favicon links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,31 +8,27 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200;300;400;500&display=swap"
           rel="stylesheet"
         />
-        <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-        <link href="/static/favicons/site.webmanifest" rel="manifest" />
+        <link href="/img/favicon.ico" rel="shortcut icon" />
+        <link href="/img/meta/site.webmanifest" rel="manifest" />
         <link
-          href="/static/favicons/apple-touch-icon.png"
+          href="/img/meta/apple-touch-icon.png"
           rel="apple-touch-icon"
           sizes="180x180"
         />
         <link
-          href="/static/favicons/favicon-32x32.png"
+          href="/img/meta/favicon-32x32.png"
           rel="icon"
           sizes="32x32"
           type="image/png"
         />
         <link
-          href="/static/favicons/favicon-16x16.png"
+          href="/img/meta/favicon-16x16.png"
           rel="icon"
           sizes="16x16"
           type="image/png"
         />
         <meta content="#18181D" name="theme-color" />
         <meta content="#18181D" name="msapplication-TileColor" />
-        <meta
-          content="/static/favicons/browserconfig.xml"
-          name="msapplication-config"
-        />
       </Head>
       <body className="bg-white dark:bg-black text-white dark:text-black font-mono antialiased">
         <Main />
