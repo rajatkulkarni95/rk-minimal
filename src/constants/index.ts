e645ad4f6f4c3aaef8cb37676e806f93a072a2d1
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-02-27 19:37:53 +0530

    fetcher function

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 export const CURRENT_LOCATION = "Mumbai, IN";
 export const COORDINATES = { latitude: 19.2145334, longitude: 72.9851942 };
+export const CURRENT_WEATHER_BASE_ENDPOINT =
+  "https://api.openweathermap.org/data/2.5/weather?";
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,14 @@ export interface Size {
   width: number | undefined;
   height: number | undefined;
 }
+
+export type TWeather = {
+  main: {
+    temp: number;
+  };
+  weather: [
+    {
+      main: string;
+    }
+  ];
+};
