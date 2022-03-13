af92cd378cc23c3244c2a1650cdcb05993a2f58d
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-03-13 11:50:35 +0530

    Removed the Container common (#4)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,9 @@
-import { Container } from "@styles/common";
 import { NextPage } from "next";
 import Link from "next/link";
 
 const ErrorPage: NextPage = () => {
   return (
-    <Container>
+    <div className="w-full h-full px-4 pb-4 mx-auto md:w-3/4 md:mt-0 md:pb-10 lg:pb-16 lg:w-[720px] lg:h-[600px] xl:my-auto">
       <h3 className="text-md my-16 text-center font-medium text-gray-200 xl:text-lg">
         Look, between the two of us, you really shouldn't be here!
       </h3>
@@ -15,7 +14,7 @@ const ErrorPage: NextPage = () => {
           </a>
         </Link>
       </section>
-    </Container>
+    </div>
   );
 };
 
