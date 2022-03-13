ec2511529db2e8aa1f60494a3484cef0b4673881
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-03-13 11:38:13 +0530

    Fix Children props

--- src/styles/common.tsx
+++ src/styles/common.tsx
@@ -4,8 +4,8 @@ type TContainerProps = {
   children: ReactNode;
 };
 
-export const Container = ({ children }: TContainerProps) => (
-  <div className="mx-auto h-full w-full px-4 pb-4 md:mt-0 md:w-3/4 md:pb-10 lg:h-[600px] lg:w-[720px] lg:pb-16 xl:my-auto">
-    {children}
+export const Container = (props: TContainerProps) => (
+  <div className="w-full h-full px-4 pb-4 mx-auto md:w-3/4 md:mt-0 md:pb-10 lg:pb-16 lg:w-[720px] lg:h-[600px] xl:my-auto">
+    {props.children}
   </div>
 );
