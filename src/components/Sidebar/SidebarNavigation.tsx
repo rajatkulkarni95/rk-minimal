9a6dc6ea3a9bd4ad9f893221579249e98c42383e
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-03-25 19:47:50 +0530

    Add links to sidebar with icons

--- src/components/Sidebar/SidebarNavigation.tsx
+++ src/components/Sidebar/SidebarNavigation.tsx
@@ -19,17 +19,20 @@ const SidebarNavigation = ({ hideSidebar }: TProps) => {
       </section>
       <section className="mt-auto px-4 w-full flex flex-col">
         <p className="text-xs text-gray-100 mb-2">On the web</p>
-        {links.slice(1).map((link) => (
-          <a
-            href={link.url}
-            key={link.id}
-            target="_blank"
-            rel="noreferrer"
-            className="py-2 px-4 rounded-md text-sm text-left cursor-pointer bg-zinc-800 text-zinc-50 hover:bg-zinc-700 w-full mb-3"
-          >
-            {link.name}
-          </a>
-        ))}
+        {links.slice(1).map((link) => {
+          const { icon: LinkIcon } = link;
+          return (
+            <a
+              href={link.url}
+              key={link.id}
+              target="_blank"
+              rel="noreferrer"
+              className="flex items-center py-2 px-4 rounded-md text-sm text-left cursor-pointer bg-zinc-800 text-zinc-50 w-full mb-3"
+            >
+              <LinkIcon /> <span className="ml-4">{link.name}</span>
+            </a>
+          );
+        })}
       </section>
     </div>
   );
--- src/data/links/index.ts
+++ src/data/links/index.ts
@@ -1,27 +1,38 @@
+import EmailIcon from "@svg/email.svg";
+import TwitterIcon from "@svg/twitter.svg";
+import LinkedinIcon from "@svg/linkedin.svg";
+import CVIcon from "@svg/cv.svg";
+import GithubIcon from "@svg/github.svg";
+
 export const links = [
   {
     id: 1,
     name: "Email",
     url: "mailto:rajatkulkarni95@gmail.com",
+    icon: EmailIcon,
   },
   {
     id: 2,
     name: "Twitter",
     url: "https://twitter.com/JokingRajat",
+    icon: TwitterIcon,
   },
   {
     id: 3,
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/rajatkulkarni95/",
+    icon: LinkedinIcon,
   },
   {
     id: 4,
     name: "Github",
     url: "https://github.com/rajatkulkarni95",
+    icon: GithubIcon,
   },
   {
     id: 5,
     name: "CV",
     url: "https://read.cv/rajatk",
+    icon: CVIcon,
   },
 ];
