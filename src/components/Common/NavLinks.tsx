dcd1fcfa9ec742e758f198d9290124151cb7c3aa
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-03-25 22:55:57 +0530

    Show navlinks only for desktop view

--- src/components/Common/NavLinks.tsx
+++ src/components/Common/NavLinks.tsx
@@ -1,6 +1,13 @@
+import useWindowSize from "src/hooks/useWindowSize";
+import { Size } from "src/types";
 import { InternalHeaderLink } from "./LinkButton";
 
 const NavLinks = () => {
+  const windowSize: Size = useWindowSize();
+  const isMobile = (windowSize?.width ?? 1026) < 1025;
+
+  if (isMobile) return null;
+
   return (
     <section className="absolute left-8 flex flex-col top-1/2 -translate-y-1/2">
       <InternalHeaderLink href="/" text="Home" />
