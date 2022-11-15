30f760829bb266dc2f5e530ad4f23bd3f65b6c25
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-11-15 21:50:15 +0530

    responsive

--- src/components/Me/index.tsx
+++ src/components/Me/index.tsx
@@ -1,27 +1,27 @@
 const Me = () => {
   return (
     <article className="flex flex-col mt-10">
-      <p className="text-xl font-normal text-primary inline-block">
+      <p className="text-base lg:text-xl font-normal text-primary inline-block">
         Currently responsible for all things frontend at{" "}
         <a
           href="https://avoma.com"
           target="_blank noopener noreferrer"
-          className="text-base text-primary py-1 px-2 rounded-md bg-red-500/70 delay-150 hover:bg-red-500/90 duration-300"
+          className="text-sm lg:text-base text-primary py-1 px-2 rounded-md bg-red-500/70 delay-150 hover:bg-red-500/90 duration-300"
         >
           Avoma
         </a>{" "}
         , helping build enriching meeting experiences.
       </p>
-      <p className="text-xl font-normal text-primary mt-6">
+      <p className="text-base lg:text-xl font-normal text-primary mt-6">
         Work with React, Typescript and Python. Learning Go and Rust via
         building.
       </p>
-      <p className="text-xl font-normal text-primary mt-12">
+      <p className="text-base lg:text-xl font-normal text-primary mt-12">
         Building{" "}
         <a
           href="https://octarine.app"
           target="_blank noopener noreferrer"
-          className="text-base text-primary py-1 px-2 rounded-md bg-violet-700/70 delay-150 hover:bg-violet-700/90 duration-300"
+          className="text-sm lg:text-base text-primary py-1 px-2 rounded-md bg-violet-700/70 delay-150 hover:bg-violet-700/90 duration-300"
         >
           Octarine
         </a>{" "}
